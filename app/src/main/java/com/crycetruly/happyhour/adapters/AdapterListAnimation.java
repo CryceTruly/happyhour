8121c3b94625492da6089efb999ccf454464b716
crycetruly <crycetruly@gmail.com>
2019-01-29 18:53:53 +0300

    Add Animation Effect to HH List

---
+++ app/src/main/java/com/crycetruly/happyhour/adapters/AdapterListAnimation.java
@@ -0,0 +1,111 @@
+package com.crycetruly.happyhour.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.crycetruly.happyhour.R;
+import com.crycetruly.happyhour.model.HappyHour;
+import com.crycetruly.happyhour.utils.ItemAnimation;
+import com.crycetruly.happyhour.utils.Tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterListAnimation extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<HappyHour> items = new ArrayList<>();
+
+    private Context ctx;
+    private OnItemClickListener mOnItemClickListener;
+    private int animation_type = 0;
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, HappyHour obj, int position);
+    }
+
+    public void setOnItemClickListener(final OnItemClickListener mItemClickListener) {
+        this.mOnItemClickListener = mItemClickListener;
+    }
+
+    public AdapterListAnimation(Context context, List<HappyHour> items, int animation_type) {
+        this.items = items;
+        ctx = context;
+        this.animation_type = animation_type;
+    }
+
+    public class OriginalViewHolder extends RecyclerView.ViewHolder {
+        public ImageView image;
+        public TextView name;
+        public View lyt_parent;
+
+        public OriginalViewHolder(View v) {
+            super(v);
+            image = (ImageView) v.findViewById(R.id.image);
+            name = (TextView) v.findViewById(R.id.name);
+            lyt_parent = (View) v.findViewById(R.id.lyt_parent);
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder vh;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_happy_hr_list, parent, false);
+        vh = new OriginalViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+        Log.e("onBindViewHolder", "onBindViewHolder : " + position);
+        if (holder instanceof OriginalViewHolder) {
+            OriginalViewHolder view = (OriginalViewHolder) holder;
+
+            HappyHour p = items.get(position);
+            view.name.setText(position + " | " + p.title);
+            view.lyt_parent.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (mOnItemClickListener != null) {
+                        mOnItemClickListener.onItemClick(view, items.get(position), position);
+                    }
+                }
+            });
+            setAnimation(view.itemView, position);
+        }
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                on_attach = false;
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+        });
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    private int lastPosition = -1;
+    private boolean on_attach = true;
+
+    private void setAnimation(View view, int position) {
+        if (position > lastPosition) {
+            ItemAnimation.animate(view, on_attach ? position : -1, animation_type);
+            lastPosition = position;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/crycetruly/happyhour/adapters/GenericAdapter.java
@@ -0,0 +1,10 @@
+package com.crycetruly.happyhour.adapters;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Elia on 18/06/2018.
+ */
+
+public class GenericAdapter  {
+}
